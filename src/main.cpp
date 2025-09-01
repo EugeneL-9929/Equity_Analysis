21b42f591f51d9b9e2153abe825b498bf56ed20f
Eugene UST <yliujz@connect.ust.hk>
2025-09-01 17:34:56 +0800

    change at 1734 20250901

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include "stock.cpp"
+
+
+using namespace std;
+
+int main(){
+    cout << "hello, world!" << endl;
+    SD::Stock stock{"QQQ"};
+    return 0;
+}
