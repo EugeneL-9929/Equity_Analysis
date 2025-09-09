a5392dc7535de1d77043066c08473d65a3cd2083
EugeneL-9929 <yliujz@connect.ust.hk>
2025-09-09 10:41:23 +0800

    change at 1041 20250909

--- src/structure.cpp
+++ src/structure.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "nlohmann/json.hpp"
 using namespace std;
+
+struct TreeNode
+{
+public:
+    TreeNode(string value) {}
+};
 
 struct Tree
 {
 public:
-    Tree()
+    Tree(int layerNumber, int serialNumber)
     {
         cout << "" << endl;
     }
@@ -15,18 +22,22 @@ public:
     {
         nlohmann::json dataJson;
         dataJson = {};
+        return dataJson;
     }
 
     void addChild()
     {
     }
 
-    static Tree loadJson(nlohmann::json dataJson)
+    static TreeNode loadJson(nlohmann::json dataJson)
     {
+        TreeNode rootNode{dataJson["layerNumber"]};
+        return rootNode;
     }
 
 private:
     int layerNumber;
     int serialNumber;
+    TreeNode *parentNode;
     vector<Tree> children{};
 };
